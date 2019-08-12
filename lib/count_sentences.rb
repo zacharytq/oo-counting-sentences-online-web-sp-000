2bc8e935c73ad63e60a183ecc8552875dce5f131
chic-page-6502 <zacharytq@gmail.com>
2019-08-12 23:45:14 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/([!?])|([^\.][\.]\z)/).length
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
